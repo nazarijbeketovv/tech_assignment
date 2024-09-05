13d85dbea0b6f7c0388296fd1ced1746f79fa9dd
Nazarij Beketov <nazarijbeketov@Nazarijs-MacBook-Air.local>
2024-09-05 15:35:57 +0300

    models were registered in admin.py

--- task_1/admin.py
+++ task_1/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Player, Boost
 
-# Register your models here.
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ("user", "points", "first_login", "last_login")
+    search_fields = ("user__username", "points")
+    list_filter = ("first_login", "last_login")
+    ordering = ("-points", "-last_login")
+
+
+@admin.register(Boost)
+class BoostAdmin(admin.ModelAdmin):
+    list_display = ("player", "boost_type", "duration", "activated_at", "is_active")
+    search_fields = ("player__user__username", "boost_type")
+    list_filter = ("boost_type", "is_active")
+    ordering = ("-activated_at", "duration")
+    readonly_fields = ("activated_at",)
--- task_2/admin.py
+++ task_2/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import Player, Level, Prize, PlayerLevel, LevelPrize
 
-# Register your models here.
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ("id", "player_id")
+    search_fields = ("player_id",)
+    ordering = ("id",)
+
+
+@admin.register(Level)
+class LevelAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "order")
+    search_fields = ("title",)
+    list_filter = ("order",)
+    ordering = ("order",)
+
+
+@admin.register(Prize)
+class PrizeAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+    search_fields = ("title",)
+    ordering = ("title",)
+
+
+class LevelInline(admin.TabularInline):
+    model = PlayerLevel
+    extra = 1
+    fields = ("level", "completed", "is_completed", "score")
+    readonly_fields = ("completed",)
+
+
+@admin.register(PlayerLevel)
+class PlayerLevelAdmin(admin.ModelAdmin):
+    list_display = ("player", "level", "completed", "is_completed", "score")
+    search_fields = ("player__player_id", "level__title")
+    list_filter = ("is_completed", "level")
+    ordering = ("-completed", "score")
+
+
+class LevelPrizeInline(admin.TabularInline):
+    model = LevelPrize
+    extra = 1
+    fields = ("prize", "received")
+    readonly_fields = ("received",)
+
+
+@admin.register(LevelPrize)
+class LevelPrizeAdmin(admin.ModelAdmin):
+    list_display = ("level", "prize", "received")
+    list_filter = ("level", "prize")
+    search_fields = ("level__title", "prize__title")
+    ordering = ("-received",)
